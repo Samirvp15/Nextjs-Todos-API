1ee571b72274fbaa0dd6049a775d073c48dcd8fc
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-20 16:31:42 -0500

    add pagination of Prisma for GetTodos

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,9 +2,21 @@
 import prisma from '@/app/lib/prisma'
 import { NextResponse } from 'next/server'
 
-export async function GET() { 
+export async function GET(request: Request) {
 
-    const todos = await prisma.todo.findMany()
+    const { searchParams } = new URL(request.url)
+    const take = +(searchParams.get('take') ?? '10')
+    const skip = +(searchParams.get('skip') ?? '0')
+    if (isNaN(take)) {
+        return NextResponse.json({ message: 'Invalid take parameter' }, { status: 400 })
+    }
+    if (isNaN(skip)) {
+        return NextResponse.json({ message: 'Invalid skip parameter' }, { status: 400 })
+    }
+    const todos = await prisma.todo.findMany({
+        take: take,
+        skip: skip,
+    })
 
-  return NextResponse.json(todos)
+    return NextResponse.json(todos)
 }
