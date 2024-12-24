4cd8d03f1b70cd1371cf836335b714416bc94e28
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-12-23 19:56:14 -0500

    add ProductsinCarT stylized

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,4 +1,5 @@
 import { cookies } from 'next/headers'
+import Link from 'next/link'
 import React from 'react'
 import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci'
 
@@ -49,14 +50,14 @@ export default async function TopMenu() {
                     <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
                         <CiChat1 size={25} />
                     </button>
-                    <button className=" p-2 flex items-center justify-center  h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+                    <Link href={'/dashboard/cart'} className=" p-2 flex items-center justify-center  h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
                         {
                             totalItems > 0 && (
                                 <span className=' text-blue-800 font-bold text-sm mr-2'>{totalItems}</span>
                             )
                         }
                         <CiShoppingBasket size={25} />
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
